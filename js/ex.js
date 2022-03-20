50a2c88752b403c8058a88754ceff213a42fe899
Dima <dima.cev30@gmai.com>
2022-03-20 15:22:05 +0300

    if and caase

--- js/ex.js
+++ js/ex.js
@@ -1,21 +1,38 @@
 "use strict";
 
-const number0films = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (5 == 4) {
+    console.log('ok');
+} else {
+    console.log('error');
+}
 
-const personalMovieDB = {
-    count: number0films,
-    movies: {},
-    across: {},
-    genres: [],
-    privat: false
-};
+const num1 = 50;
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+if (num1 < 49) {
+    console.log('error');
+} else if (num1 > 100) {
+    console.log('много');
+} else {
+    console.log('ok');
+}
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+const num = 50;
 
-console.log(personalMovieDB);
+(num === 50) ? console.log('ok') : console.log('error');
+
+const num2 = 51;
+
+switch (num2) {
+    case 49: 
+        console.log('error');
+        break;
+    case 100:
+        console.log('error');
+        break;
+    case 50:
+        console.log('right');
+        break;
+    default:
+        console.log('not now');
+        break;
+}
