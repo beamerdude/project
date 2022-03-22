d11060370174a761308909d216ef5cfdeeab399c
Dima <dima.cev30@gmai.com>
2022-03-22 20:31:09 +0300

    js practic 2

--- js/ex.js
+++ js/ex.js
@@ -1,38 +1,47 @@
 "use strict";
 
-if (5 == 4) {
-    console.log('ok');
-} else {
-    console.log('error');
+const number0films = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: number0films,
+    movies: {},
+    across: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Какой последний фильм вы смотрели', ''),
+          b = prompt('оцените фильм', '');
+    
+          if (a != null && b != null && a != " " && b != " " && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+          } else {
+              console.log('error');
+              i--;
+          }
+
 }
 
-const num1 = 50;
-
-if (num1 < 49) {
-    console.log('error');
-} else if (num1 > 100) {
-    console.log('много');
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов')
+} else if (10 < personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман')
 } else {
-    console.log('ok');
+    console.log('Произошла ошибка')
 }
 
-const num = 50;
+console.log(personalMovieDB);
 
-(num === 50) ? console.log('ok') : console.log('error');
+function game() {
+    console.log('you win');
+}
 
-const num2 = 51;
+game();
 
-switch (num2) {
-    case 49: 
-        console.log('error');
-        break;
-    case 100:
-        console.log('error');
-        break;
-    case 50:
-        console.log('right');
-        break;
-    default:
-        console.log('not now');
-        break;
+const y = function squiz() {
+    console.log('you lose');
 }
